f6e26efa0b05da1c535efbebb269280269056d6a
vikasya7 <vikasyaa19@gmail.com>
2025-05-17 10:56:46 +0000

    dataconversion

---
+++ 01_basics/03_conversionoperation.js
@@ -0,0 +1,25 @@
+// let score = true
+// console.log(typeof score);
+// let valueInnumber=Number(score)
+// console.log(typeof valueInnumber);
+// console.log(valueInnumber);
+
+// "33"-->33
+// "33abc"-->NaN(not a number)
+// true-->1, false-->0
+
+// let isloggedIn= 1
+
+// let booleanisloggedin=Boolean(isloggedIn)
+// console.log(booleanisloggedin);
+
+//""-->false "vikas"-->true
+let somenumber=33
+let stringnumber=String(somenumber)
+console.log(stringnumber);
+console.log(typeof stringnumber);
+
+
+
+
+
