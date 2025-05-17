deeb818292917839189373c2698e6f061e62ce1b
vikasya7 <vikasyaa19@gmail.com>
2025-05-17 06:16:39 +0000

    datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,24 @@
+"use strict"; // treat all js as new version
+
+//alert(3 + 3) we are using node js not browser
+
+console.log(3+3) // code readebility
+
+let name="vikas"
+let age =18
+let isLoggedin = false
+let state;
+
+// number= 2^53
+// bigInt
+// string= ""
+// boolean = true or false
+// null = standalone value
+// undefined
+// symbol = unique
+
+
+// object
+
+console.log(typeof null); // object
+console.log(typeof undefined); // undefined
