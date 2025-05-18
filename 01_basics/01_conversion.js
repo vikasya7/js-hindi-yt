7d3d18413c07b5301db22d5af75f88d4dbe1e928
vikasya7 <vikasyaa19@gmail.com>
2025-05-18 10:50:38 +0000

    conversion done

---
+++ 01_basics/01_conversion.js
@@ -0,0 +1,13 @@
+// console.log("2">1);
+console.log(null>0);
+console.log(null==0);
+console.log(null>=0);
+
+console.log(undefined>0);
+console.log(undefined==0); // avoid these comparisions
+console.log(undefined<0);
+
+
+console.log("2"==2);
+console.log("2"===2); // strict checking
+
