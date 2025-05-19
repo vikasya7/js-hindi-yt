392ac3ec15b93a9edfadfda430a404c9cdcab57d
vikasya7 <vikasyaa19@gmail.com>
2025-05-19 16:01:23 +0000

    maths

---
+++ 01_basics/num_maths.js
@@ -0,0 +1,26 @@
+const score=400
+
+const balance=new Number(100)
+// console.log(balance);
+// console.log(balance.toString().length);
+// console.log(balance.toFixed(3));
+
+const otherNumber=123.8966
+//console.log(otherNumber.toPrecision(4));
+
+const hundred=1000000
+//console.log(hundred.toLocaleString('en-IN'));
+
+
+//++++++++++++++++maths++++++++++++++
+// console.log(Math);
+// console.log(Math.abs(-4));
+// console.log(Math.round(4.5));
+// console.log(Math.ceil(4.2));
+// console.log(Math.floor(4.9));
+
+console.log(Math.random());
+console.log(Math.random()*10);
+
+
+
