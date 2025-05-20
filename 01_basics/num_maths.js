16f70a71a2fafa6a093d2bca40fa60eb64347de5
vikasya7 <vikasyaa19@gmail.com>
2025-05-20 17:10:13 +0530

    Update num_maths.js

--- 01_basics/num_maths.js
+++ 01_basics/num_maths.js
@@ -21,6 +21,12 @@ const hundred=1000000
 
 console.log(Math.random());
 console.log(Math.random()*10);
+console.log(Math.floor((Math.random()*10))+1);
+
+
+const min=10
+const max =20
+console.log(Math.floor(Math.random() *(max-min+1))+min)
 
 
 
